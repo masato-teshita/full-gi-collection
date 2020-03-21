d84bc597cccf46736901926680a38a032661ae00
m.teshita <masato.t2207@gmail.com>
2020-03-22 00:22:16 +0900

    口コミタイプのアイコン表示ヘルパーメソッドの追加

--- app/helpers/woms_helper.rb
+++ app/helpers/woms_helper.rb
@@ -1,2 +1,10 @@
 module WomsHelper
+  def visit_type(wom)
+    if wom.visit_type == "購入"
+      content_tag :i, '', class: "fas fa-tshirt"
+    elsif wom.visit_type == "売却"
+      content_tag :i, '', class: "fas fa-yen-sign"
+    else
+    end
+  end
 end
