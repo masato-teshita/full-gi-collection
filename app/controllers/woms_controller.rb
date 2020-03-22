bb831768e96a1c10c728b75dd560c80aa64f0699
m.teshita <masato.t2207@gmail.com>
2020-03-22 21:24:43 +0900

    各ビューにインスタンスを渡せるように定義

--- app/controllers/woms_controller.rb
+++ app/controllers/woms_controller.rb
@@ -13,11 +13,39 @@ class WomsController < ApplicationController
       render template: 'shops/woms'
     end
   end
-  
-  def create
-    @wom = Wom.create(wom_params)
+
+  def new
+    @wom = Wom.new
+    @shop = Shop.find(params[:shop_id])
+    @woms = @shop.woms.where.not(rate: nil).order("created_at DESC").page(params[:page]).per(10)
+    @all_woms = @shop.woms.where.not(rate: nil)
+    @users = @shop.users
   end
 
+  def create
+    wom = Wom.create(wom_params)
+    redirect_to shop_woms_path(params[:shop_id])
+  end
+
+  def edit
+    @wom = Wom.find(params[:id])
+    @shop = Shop.find(params[:shop_id])
+    @woms = @shop.woms.where.not(rate: nil).order("created_at DESC").page(params[:page]).per(10)
+    @all_woms = @shop.woms.where.not(rate: nil)
+    @users = @shop.users
+  end
+
+  def update
+    wom = Wom.find(params[:id])
+    wom.update(wom_params)
+    redirect_to shop_woms_path(params[:shop_id])
+  end
+
+  def destroy
+    wom = Wom.find(params[:id])
+    wom.destroy
+    redirect_to shop_woms_path(params[:shop_id])
+  end
   private
   def wom_params
     params.require(:wom).permit(:title, :content, :rate, :visit_type, :visit_date).merge(user_id: current_user.id, shop_id: params[:shop_id])
