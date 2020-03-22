04eff223eabdfc728e604b517219b1156f2c40ab
m.teshita <masato.t2207@gmail.com>
2020-03-22 22:18:09 +0900

    historiesコントローラーのindexアクションの設定

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,7 @@
 class HistoriesController < ApplicationController
   def index
+    @user = current_user
+    @histories = History.where(user_id: current_user).order(created_at: "DESC").page(params[:page]).per(10)
   end
 
   def create
