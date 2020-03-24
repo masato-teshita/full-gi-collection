4e0466f35107f8fb31a314649e0f824298b59734
m.teshita <masato.t2207@gmail.com>
2020-03-24 22:13:22 +0900

    マイグレーションファイルの修正（clipsテーブルの項目設定）

--- db/migrate/20200324130814_create_clips.rb
+++ db/migrate/20200324130814_create_clips.rb
@@ -1,7 +1,8 @@
 class CreateClips < ActiveRecord::Migration[5.0]
   def change
     create_table :clips do |t|
-
+      t.references :user, foreign_key: true
+      t.references :shop, foreign_key: true
       t.timestamps
     end
   end
