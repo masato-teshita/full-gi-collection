c33ff7df33c61b588abca94c86a52f00522008d0
m.teshita <masato.t2207@gmail.com>
2020-03-24 21:39:16 +0900

    口コミ投稿時のバリデーションの設定

--- app/models/wom.rb
+++ app/models/wom.rb
@@ -1,6 +1,12 @@
 class Wom < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+
+  validates :visit_type, presence: true
+  validates :rate, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
+
   def self.search(visit_type, shop_id)
     if visit_type != ""
       Wom.where('(visit_type = ?) AND (shop_id = ?)', "#{visit_type}", "#{shop_id}")
