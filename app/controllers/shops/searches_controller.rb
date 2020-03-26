1c2f6439cce8e02f9203d32a87e9582453f50deb
m.teshita <masato.t2207@gmail.com>
2020-03-26 22:32:24 +0900

    コンフリクトの解消

--- app/controllers/shops/searches_controller.rb
+++ app/controllers/shops/searches_controller.rb
@@ -1,9 +1,9 @@
 class Shops::SearchesController < ApplicationController
   def index
-    if params[:keyword].present?
-      @shops = Shop.search(params[:keyword])
-    elsif params[:area_id].present?
+    if params[:area_id].present?
       @shops = Shop.where(area_id: params[:area_id])
+    else
+      @shops = Shop.search(params[:keyword])
     end
   end
 end
