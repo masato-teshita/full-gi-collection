79f418bd1735d139b64ff5bcb045b7628a8caec8
m.teshita <masato.t2207@gmail.com>
2020-03-26 21:40:53 +0900

    ビューに渡すインスタンスの定義

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,9 @@
 class TopsController < ApplicationController
   def index
-    render layout: false
     @shop = Shop.new
+    @shops = Shop.all.order(:id)
+    @areas = Area.all.order(:id)
+    @top_areas = Area.limit(5).order_by_shops
+    render layout: false
   end
 end
