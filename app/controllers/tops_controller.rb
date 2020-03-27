362cb877409d60b263dcec90414f027ec818a799
m.teshita <masato.t2207@gmail.com>
2020-03-27 22:36:17 +0900

    topビューに渡すインスタンスの定義

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,8 +2,10 @@ class TopsController < ApplicationController
   def index
     @shop = Shop.new
     @shops = Shop.all.order(:id)
-    @areas = Area.all.order(:id)
     @top_areas = Area.limit(5).order_by_shops
+    @areas = Area.all.order(:id)
+    @top_genres = Genre.limit(5).order_by_shops
+    @genres = Genre.all.order(:id)
     render layout: false
   end
 end
