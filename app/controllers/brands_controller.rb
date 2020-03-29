46b3e0cd2c6dbfa89c9b19ce58ad5a5953ee1630
m.teshita <masato.t2207@gmail.com>
2020-03-29 22:13:21 +0900

    キーワード入力の都度、入力ワードにヒットするブランドとジャンル名をインスタンスとしてビューに返すメソッドの追加

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -1,5 +1,12 @@
 class BrandsController < ApplicationController
   def index
-    @brands = Brand.all
+    keyword = params[:keyword]
+    return nil if keyword == ""
+    brands = Brand.where(['name ilike ?', "%#{keyword}%"]).limit(5)
+    genres = Genre.where(['name ilike ?', "%#{keyword}%"])
+    @keywords = brands + genres
+    respond_to do |format|
+      format.json
+    end
   end
 end
