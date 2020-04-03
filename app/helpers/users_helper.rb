6bd5bba14f6c752d71c6db8d356f4e640d2c63b3
m.teshita <masato.t2207@gmail.com>
2020-04-03 22:22:24 +0900

    ユーザーアイコンのデフォルトアイコンを設定。また、背景画像も登録されていない場合は、デフォルト画像を表示するようにメソッドを追加。

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,35 @@
 module UsersHelper
   def user_image_present?(user)
     if user.image.present?
-      image_tag user.image.url, class: 'user-icon-img'
+      link_to user_path(user.id) do
+        image_tag user.image.url, class: 'user-icon-img'
+      end
     else
       image_tag '/assets/no-image.png', class: 'user-icon-img'
+    end
+  end
+
+  def user_icon_present?(user)
+    if user.image.present?
+      image_tag user.image.url, class: 'user-nav-icon js-user-nav'
+    else
+      image_tag '/assets/default-icon.png', class: 'user-nav-icon js-user-nav'
+    end
+  end
+
+  def user_cover_image_present?(user)
+    if user.cover_image.present?
+      image_tag user.cover_image, id: 'user-cover-img-prev'
+    else
+      image_tag '/assets/furugi-top.jpeg', id: 'user-cover-img-prev'
+    end
+  end
+
+  def user_cover_icon_present?(user)
+    if user.cover_image.present?
+      image_tag user.cover_image, class: 'cover-img'
+    else
+      image_tag '/assets/furugi-top.jpeg', class: 'cover-img'
     end
   end
 end
