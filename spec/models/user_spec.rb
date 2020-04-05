36c681aa55ad92f0f451c608dbcb09a329fe06bb
m.teshita <masato.t2207@gmail.com>
2020-04-05 20:05:19 +0900

    Userモデルのテスト：name/email/password/password_confirmationが存在すればできることのexampleを追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "name/email/password/password_confirmationが存在すれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+  end
+
+end
