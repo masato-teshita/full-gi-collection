fda37105a266cd8aa6308f70e9b541c986413ef2
m.teshita <masato.t2207@gmail.com>
2020-04-05 20:49:59 +0900

    Womモデルのテスト：visit_typeがない場合は登録できないことのexampleを追加

--- spec/models/wom_spec.rb
+++ spec/models/wom_spec.rb
@@ -6,6 +6,12 @@ describe Wom do
       expect(wom).to be_valid
     end
 
+    it "visit_typeがない場合は登録できないこと" do
+      wom = build(:wom, visit_type: "")
+      wom.valid?
+      expect(wom.errors[:visit_type]).to include("を入力してください")
+    end
+
   end
 
 end
