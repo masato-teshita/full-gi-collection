87c8d943bc1c7815b7e1325473a5cb5c28ba9e4a
m.teshita <masato.t2207@gmail.com>
2020-04-05 15:52:47 +0900

    userモデルのFactoryBotの定義

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  
+  factory :user do
+    name                  {"teshita"}
+    email                 {"test@teshita.com"}
+    password              {"000000"}
+    password_confirmation {"000000"}
+  end
+end
