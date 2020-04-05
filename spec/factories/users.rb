dc3b8a7612561c68935d52a49871c194b474207d
m.teshita <masato.t2207@gmail.com>
2020-04-05 23:06:32 +0900

    Userクラスのfactoryの定義を修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   
   factory :user do
     password = Faker::Internet.password(min_length: 6)
-    sequence(:name) {Faker::Name.name}
-    sequence(:email) {Faker::Internet.email}
+    name {Faker::Name.name}
+    email {Faker::Internet.email}
     password {password}
     password_confirmation {password}
   end
