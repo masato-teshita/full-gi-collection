d66016e6bda2b893a345e50c2d8e66426f57dfa3
m.teshita <masato.t2207@gmail.com>
2020-04-11 16:06:59 +0900

    ユーザーマイページのレイアウトずれの調整

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,7 @@
 module UsersHelper
   def user_image_present?(user)
     if user.image.present?
-      link_to user_path(user.id) do
-        image_tag user.image.url, class: 'user-icon-img'
-      end
+      image_tag user.image.url, class: 'user-icon-img'
     else
       image_tag '/assets/no-image.png', class: 'user-icon-img'
     end
