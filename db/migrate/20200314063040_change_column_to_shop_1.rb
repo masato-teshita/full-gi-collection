d85ea772b8125353e6316d25e80c5586a74a3473
m.teshita <masato.t2207@gmail.com>
2020-04-11 17:28:27 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20200314063040_change_column_to_shop_1.rb
+++
@@ -1,9 +0,0 @@
-class ChangeColumnToShop < ActiveRecord::Migration[5.0]
-  def up
-    change_column :shops, :image, :string, unique: true
-  end
-
-  def down
-    change_column :shops, :image, :string
-  end
-end
