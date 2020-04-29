eb3719428d1cfd87b630848334213870ab313804
m.teshita <masato.t2207@gmail.com>
2020-04-30 01:28:13 +0900

    historiesコントローラ・clipsコントローラにskip_before_actionを追記
    ログインしていなくても、各ユーザーページの「行ったお店」「保存済み」を閲覧できるように、skip_before_actionを追記

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,4 +1,6 @@
 class ClipsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+  
   def index
     @user = User.find(params[:user_id])
     clips = Clip.where(user_id: @user).order(created_at: "DESC")
--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,4 +1,6 @@
 class HistoriesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @user = User.find(params[:user_id])
     histories = History.where(user_id: @user.id).order(created_at: "DESC")
