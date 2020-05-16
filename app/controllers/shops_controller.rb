c34e4ba2c8010289a7c998f229d885b1b1e30d36
m.teshita <masato.t2207@gmail.com>
2020-05-17 01:31:58 +0900

    shops/createアクションで保存に失敗した場合にrender :newするように修正
    同時に、strong parameterでaddressカラムの値も許可

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -15,7 +15,8 @@ class ShopsController < ApplicationController
   end
 
   def create
-    Shop.create!(shop_params)
+    @shop = Shop.new(shop_params)
+    @shop.save! ? (redirect_to root_path) : (render :new)
   end
 
   def show
@@ -25,10 +26,10 @@ class ShopsController < ApplicationController
     @clip = Clip.find_by(user_id: current_user, shop_id: @shop.id)
     @clips = @shop.clips
   end
-
+  
   private
   def shop_params
-    params.require(:shop).permit(:name, :image, :outline)
+    params.require(:shop).permit(:name, :image, :outline, :address)
   end
 
   def move_to_root    
