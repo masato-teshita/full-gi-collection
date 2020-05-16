59922af46499c373c8d1d14ff55e32b190bf5732
m.teshita <masato.t2207@gmail.com>
2020-05-16 23:38:12 +0900

    shopsテーブルに、住所と緯度経度のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200516070449) do
+ActiveRecord::Schema.define(version: 20200516160300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,16 +53,6 @@ ActiveRecord::Schema.define(version: 20200516070449) do
     t.index ["user_id"], name: "index_histories_on_user_id", using: :btree
   end
 
-  create_table "maps", force: :cascade do |t|
-    t.integer  "shop_id"
-    t.string   "address",    null: false
-    t.float    "latitude",   null: false
-    t.float    "longitude",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_maps_on_shop_id", using: :btree
-  end
-
   create_table "shop_brands", force: :cascade do |t|
     t.integer "shop_id"
     t.integer "brand_id"
@@ -92,6 +82,9 @@ ActiveRecord::Schema.define(version: 20200516070449) do
     t.datetime "updated_at", null: false
     t.integer  "area_id"
     t.integer  "genre_id"
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["area_id"], name: "index_shops_on_area_id", using: :btree
     t.index ["genre_id"], name: "index_shops_on_genre_id", using: :btree
     t.index ["image"], name: "index_shops_on_image", unique: true, using: :btree
@@ -134,7 +127,6 @@ ActiveRecord::Schema.define(version: 20200516070449) do
   add_foreign_key "clips", "users"
   add_foreign_key "histories", "shops"
   add_foreign_key "histories", "users"
-  add_foreign_key "maps", "shops"
   add_foreign_key "shop_brands", "brands"
   add_foreign_key "shop_brands", "shops"
   add_foreign_key "shop_genres", "genres"
