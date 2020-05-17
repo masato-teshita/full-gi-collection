0a4d7fe771f489a46596a7d0c5988673200b18de
m.teshita <masato.t2207@gmail.com>
2020-05-17 12:28:31 +0900

    不要な記述の削除

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -86,14 +86,4 @@ class Shop < ApplicationRecord
       shop_genres << genre.name
     end
   end
-
-  private
-  def geocode
-    uri = URI.escape("https://maps.googleapis.com/maps/api/geocode/json?address="+self.address.gsub(" ", "")+"&key=#{ENV['GOOGLE_MAP_API_KEY']}}")
-    res = HTTP.get(uri).to_s
-    response = JSON.parse(res)
-    self.latitude = response["results"][0]["geometry"]["location"]["lat"]
-    self.longitude = response["results"][0]["geometry"]["location"]["lng"]
-  end
-
 end
