f942eba2efe4125fc9d410c5d8a3fa5986b3a2ac
m.teshita <masato.t2207@gmail.com>
2020-05-17 11:57:32 +0900

    本番環境のshopsテーブルにカラム追加するマイグレーションファイルの作成

---
+++ db/migrate/20200517025616_add_column_to_shop.rb
@@ -0,0 +1,7 @@
+class AddColumnToShop < ActiveRecord::Migration[5.0]
+  def change
+    add_column :shops, :address, :string
+    add_column :shops, :latitude, :float
+    add_column :shops, :longitude, :float
+  end
+end
