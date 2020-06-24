13ec55614edcb0f612418f232e337eb04785e69d
m.teshita <masato.t2207@gmail.com>
2020-06-24 22:00:32 +0900

    feat: デプロイの設定ファイルを環境に合わせて変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,6 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
+server '18.181.56.83', user: 'masato', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
