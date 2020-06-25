bb0e3590cb195fe5a18cb71f0131f187a88a2dd9
m.teshita <masato.t2207@gmail.com>
2020-06-25 23:40:06 +0900

    fix: pgからMySQLへ移行したため、検索のワイルドカードを修正

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -2,7 +2,7 @@ class AreasController < ApplicationController
   skip_before_action :authenticate_user!
   def index
     return nil if params[:area] == ""
-    @areas = Area.where(['name ilike ?', "%#{params[:area]}%"])
+    @areas = Area.where(['name like ?', "%#{params[:area]}%"])
     respond_to do |format|
       format.json
     end
