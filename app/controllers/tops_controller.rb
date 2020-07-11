c5df1f3fd5bfbbba7bc6f22208b94e039124bd64
m.teshita <masato.t2207@gmail.com>
2020-07-11 15:43:54 +0900

    feat: トップページで検索クエリのメソッドを呼び出し

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,7 @@
 class TopsController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_shop_search_query
+
   def index
     @shops = Shop.all.order(:id)
     @woms = Wom.all
