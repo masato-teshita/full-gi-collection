78c8807c2f1f1fb8e7d900873d35bedf62800668
m.teshita <masato.t2207@gmail.com>
2020-07-12 01:31:16 +0900

    fix: トップページの、ユーザに紐づくclipのN+1問題を解決

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -3,4 +3,5 @@ class Clip < ApplicationRecord
   belongs_to :shop
 
   scope :clip_includes, -> {includes(shop: [:area, :shop_genres, :genres, :shop_images])}
+  scope :top_clip_includes, -> {includes(shop: [:area, :shop_images])}
 end
