70e8dd7c63048517779aa211b48acd2c40d18808
m.teshita <masato.t2207@gmail.com>
2020-07-12 01:25:13 +0900

    fix: 行ったお店一覧でのN+1問題を解決(スコープを使用)

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -3,8 +3,7 @@ class HistoriesController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    histories = History.where(user_id: @user.id).order(created_at: "DESC")
-    @histories = histories.paginate(page: params[:page], per_page: 5)
+    @histories = History.history_includes.where(user_id: @user.id).order(created_at: "DESC").paginate(page: params[:page], per_page: 5)
   end
 
   def create
--- app/models/history.rb
+++ app/models/history.rb
@@ -1,4 +1,7 @@
 class History < ApplicationRecord
   belongs_to :user
   belongs_to :shop
+
+  scope :history_includes, -> {includes(shop: [:area, :shop_genres, :genres, :shop_images])}
+
 end
