841b69eeb48e820a7bed32772897db08708983ef
m.teshita <masato.t2207@gmail.com>
2020-07-24 16:46:30 +0900

    feat: active storageのマイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,122 +10,144 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200517035253) do
+ActiveRecord::Schema.define(version: 2020_07_23_154109) do
 
-  create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "areas", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.string   "image",      null: false
+  create_table "brands", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "clips", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "shop_id"
+  create_table "clips", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_clips_on_shop_id", using: :btree
-    t.index ["user_id"], name: "index_clips_on_user_id", using: :btree
+    t.index ["shop_id"], name: "index_clips_on_shop_id"
+    t.index ["user_id"], name: "index_clips_on_user_id"
   end
 
-  create_table "genres", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.string   "image"
+  create_table "genres", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "shop_id"
+  create_table "histories", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_histories_on_shop_id", using: :btree
-    t.index ["user_id"], name: "index_histories_on_user_id", using: :btree
+    t.index ["shop_id"], name: "index_histories_on_shop_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
-  create_table "shop_brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shop_brands", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "shop_id"
     t.integer "brand_id"
-    t.index ["brand_id"], name: "index_shop_brands_on_brand_id", using: :btree
-    t.index ["shop_id"], name: "index_shop_brands_on_shop_id", using: :btree
+    t.index ["brand_id"], name: "index_shop_brands_on_brand_id"
+    t.index ["shop_id"], name: "index_shop_brands_on_shop_id"
   end
 
-  create_table "shop_genres", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shop_genres", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "shop_id"
     t.integer "genre_id"
-    t.index ["genre_id"], name: "index_shop_genres_on_genre_id", using: :btree
-    t.index ["shop_id"], name: "index_shop_genres_on_shop_id", using: :btree
+    t.index ["genre_id"], name: "index_shop_genres_on_genre_id"
+    t.index ["shop_id"], name: "index_shop_genres_on_shop_id"
   end
 
-  create_table "shop_images", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "shop_image"
-    t.integer  "shop_id"
+  create_table "shop_images", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "shop_image"
+    t.integer "shop_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["shop_id"], name: "index_shop_images_on_shop_id", using: :btree
+    t.index ["shop_id"], name: "index_shop_images_on_shop_id"
   end
 
-  create_table "shop_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "shop_users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "shop_id"
     t.integer "user_id"
-    t.index ["shop_id"], name: "index_shop_users_on_shop_id", using: :btree
-    t.index ["user_id"], name: "index_shop_users_on_user_id", using: :btree
+    t.index ["shop_id"], name: "index_shop_users_on_shop_id"
+    t.index ["user_id"], name: "index_shop_users_on_user_id"
   end
 
-  create_table "shops", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                       null: false
-    t.text     "outline",      limit: 65535
-    t.integer  "area_id"
-    t.string   "address"
-    t.float    "latitude",     limit: 24
-    t.float    "longitude",    limit: 24
-    t.string   "phone_number"
-    t.string   "postal_code"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
-    t.index ["area_id"], name: "index_shops_on_area_id", using: :btree
+  create_table "shops", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "outline"
+    t.integer "area_id"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "phone_number"
+    t.string "postal_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["area_id"], name: "index_shops_on_area_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",                                                 null: false
-    t.string   "email",                                default: "",    null: false
-    t.string   "encrypted_password",                   default: "",    null: false
-    t.string   "image"
-    t.string   "cover_image"
-    t.text     "introduction",           limit: 65535
-    t.boolean  "admin",                                default: false
-    t.boolean  "super_admin",                          default: false
-    t.string   "reset_password_token"
+  create_table "users", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "image"
+    t.string "cover_image"
+    t.text "introduction"
+    t.boolean "admin", default: false
+    t.boolean "super_admin", default: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.datetime "created_at",                                           null: false
-    t.datetime "updated_at",                                           null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "woms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "content",    limit: 65535, null: false
-    t.float    "rate",       limit: 24,    null: false
-    t.string   "visit_type",               null: false
-    t.string   "title",                    null: false
-    t.date     "visit_date"
-    t.integer  "user_id"
-    t.integer  "shop_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["shop_id"], name: "index_woms_on_shop_id", using: :btree
-    t.index ["user_id"], name: "index_woms_on_user_id", using: :btree
+  create_table "woms", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content", null: false
+    t.float "rate", null: false
+    t.string "visit_type", null: false
+    t.string "title", null: false
+    t.date "visit_date"
+    t.integer "user_id"
+    t.integer "shop_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shop_id"], name: "index_woms_on_shop_id"
+    t.index ["user_id"], name: "index_woms_on_user_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "clips", "shops"
   add_foreign_key "clips", "users"
   add_foreign_key "histories", "shops"
