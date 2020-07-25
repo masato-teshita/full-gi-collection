3f8c7868762d0d14b996f4e4f7123adfd01b6d2d
m.teshita <masato.t2207@gmail.com>
2020-07-25 20:39:49 +0900

    fix: スペルミスの修正

--- app/models/item_status.rb
+++ app/models/item_status.rb
@@ -1,9 +1,9 @@
 class ItemStatus < ActiveHash::Base
   self.data = [
-      {id: 1, name: 'A', deinitionf: '新品・未使用'},
-      {id: 2, name: 'B', deinitionf: '未使用に近い'},
-      {id: 3, name: 'C', deinitionf: '目立った傷や汚れなし'},
-      {id: 4, name: 'D', deinitionf: '傷や汚れあり'},
-      {id: 5, name: 'E', deinitionf: '全体的に状態が悪い'},
+      {id: 1, name: 'A', definition: '新品・未使用'},
+      {id: 2, name: 'B', definition: '未使用に近い'},
+      {id: 3, name: 'C', definition: '目立った傷や汚れなし'},
+      {id: 4, name: 'D', definition: '傷や汚れあり'},
+      {id: 5, name: 'E', definition: '全体的に状態が悪い'},
   ]
 end
