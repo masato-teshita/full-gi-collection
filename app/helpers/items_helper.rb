6804866aee68758f065a74b7df5895b972953dfe
m.teshita <masato.t2207@gmail.com>
2020-07-27 22:40:16 +0900

    feat: itemsビュー周りで使用するヘルパーメソッドの定義

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def show_price(item)
+    "¥#{item.price.to_s(:delimited)}"
+  end
+  
+  def show_status(item)
+    "コンディション：#{item.item_status.name}（#{item.item_status.definition}）"
+  end
 end
