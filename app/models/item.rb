abedb48e856342f5ee8cff9a6eeaac4625f95585
m.teshita <masato.t2207@gmail.com>
2020-07-27 22:12:52 +0900

    feat: item_statusのアクティブハッシュとのアソシエーションを定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :shop
   has_many_attached :images
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :item_status
 end
