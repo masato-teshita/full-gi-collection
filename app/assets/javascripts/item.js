70d61541ae601dfa93de62201bdf0e645fbab8ec
m.teshita <masato.t2207@gmail.com>
2020-07-27 23:04:18 +0900

    feat: 商品詳細ページのアイテム説明タブとサイズタブを切り替えるjsを実装

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,6 +1,20 @@
 $(function() {
-  $('.item_box__right__detail__title--tab').on('click', function() {
-    $('.item_box__right__detail__title--tab').removeClass('selected_detail');
-    $(this).addClass('selected_detail');
+  const explanation_tab = $('#item_explanation_tab')
+  const explanation_box =$('#item_explanation_box')
+  const size_tab = $('#item_size_tab')
+  const size_box = $('#item_size_box')
+
+  explanation_tab.on('click', function() {
+    explanation_tab.addClass('selected_tab')
+    explanation_box.addClass('active_box')
+    size_tab.removeClass('selected_tab')
+    size_box.removeClass('active_box')
+  });
+
+  size_tab.on('click', function() {
+    size_tab.addClass('selected_tab')
+    size_box.addClass('active_box')
+    explanation_tab.removeClass('selected_tab')
+    explanation_box.removeClass('active_box')
   });
 });
