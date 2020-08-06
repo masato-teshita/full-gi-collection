d98d98ec168be6e560b9413621972eee6f1ae821
m.teshita <masato.t2207@gmail.com>
2020-08-07 00:07:54 +0900

    feat: itemモデルにcsvデータを読み込んでDBへ保存する処理を定義

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,25 @@ class Item < ApplicationRecord
   has_many_attached :images
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :item_status
+
+  def self.import(file, shop_id)
+    CSV.foreach(file.path, headers: true) do |row|
+      item = find_by(id: row["id"]) || new
+      item.attributes = row.to_hash.slice(*updatable_attributes)
+      item.shop_id = shop_id
+      item.save
+    end
+  end
+
+  def self.updatable_attributes
+    [
+      "id",
+      "name",
+      "item_status_id",
+      "explanation",
+      "price",
+      "size",
+      "size_detail",
+    ]
+  end
 end
